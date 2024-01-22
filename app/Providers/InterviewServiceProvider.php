c00260ce142e70e2feb00969e39b522df5b95d97
Sclifie <gleb.kolesnikov89@yandex.ru>
2024-01-22 08:24:49 +0400

    renamed service container identifier

--- app/Providers/InterviewServiceProvider.php
+++ app/Providers/InterviewServiceProvider.php
@@ -2,15 +2,17 @@
 
 namespace App\Providers;
 
+use App\Facades\InterviewFacade;
 use App\Models\Interview;
 use App\Services\InterviewService\InterviewService;
+use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 
 class InterviewServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind('interview-service', function (){
+        $this->app->bind('app-interview-service', function (){
             return new InterviewService();
         });
     }
